0228017bdeff0463f8a2a0036c1c9fe8e2bca7e3
Tamal Patra <tpatra@agl.com.au>
2020-03-24 21:21:09 +1100

    Initial upload

---
+++ SimpleFolderContents/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleFolderContents
+{
+    public partial class _Default : Page
+    {
+        private string rootDir = ConfigurationManager.AppSettings["rootDir"];
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Directory.Exists(rootDir))
+            {
+                var contents = Directory.EnumerateFiles(rootDir);
+                ddlDirectoryContents.DataSource = contents;
+                ddlDirectoryContents.DataBind();
+            }
+        }
+    }
+}
---
+++ SimpleFolderContents/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleFolderContents.Global" Language="C#" %>
